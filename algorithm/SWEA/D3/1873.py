0d7ea56a409769fbb8a6ce5094dac5b22ebca408
eora21 <eora21@naver.com>
2021-08-08 13:38:35 +0900

    class

--- algorithm/SWEA/D3/1873.py
+++ algorithm/SWEA/D3/1873.py
@@ -1,79 +1,98 @@
-T = int(input())
+class Game:
+    def __init__(self, H, W):
+        self.li = []
+        self.H = H
+        self.W = W
+        for row in range(H):
+            string = input()
+            tank =  max(string.find("^"), string.find("<"), string.find(">"), string.find("v"))
+            if tank != -1:
+                self.X = tank
+                self.Y = row
+            self.li.append(list(string))
 
-for case in range(T):
-    H, W = map(int, input().split())
-    li = [[None]] * H
-    
-    X, Y = 0, 0
-
-    for row in range(H):
-        li[row] = input()
-        tank =  max(li[row].find("^"), li[row].find("<"), li[row].find(">"), li[row].find("v"))
-        if tank != -1:
-            X = tank
-            Y = row
-        li[row] = list(li[row])
-
-    N = int(input())
-    act = input()
-    for i in range(N):
-        Bomb = False
-        _Y = Y
-        _X = X
-        if act[i] == "U":
-            li[Y][X] = "^"
+    def Command(self, N, act, t):
+        def Up():
+            self.li[Y][X] = "^"
             if Y != 0:
                 _Y = Y - 1
-        elif act[i] == "D":
-            li[Y][X] = "v"
+                Move(_Y, X)
+            
+        def Down():
+            self.li[Y][X] = "v"
             if Y != H - 1:
                 _Y = Y + 1
-        elif act[i] == "L":
-            li[Y][X] = "<"
+                Move(_Y, X)
+
+        def Left():
+            self.li[Y][X] = "<"
             if X != 0:
                 _X = X - 1
-        elif act[i] == "R":
-            li[Y][X] = ">"
+                Move(Y, _X)
+
+        def Right():
+            self.li[Y][X] = ">"
             if X != W - 1:
                 _X = X + 1
-        else:
-            Bomb = True
-            add_Y = 0
-            add_X = 0
-            if li[Y][X] == "^":
-                add_Y = -1
-            elif li[Y][X] == "v":
-                add_Y = 1
-            elif li[Y][X] == "<":
-                add_X = -1
-            else:
-                add_X = 1
+                Move(Y, _X)
+
+        def Move(_Y, _X):
+            if self.li[_Y][_X] == ".":
+                self.li[Y][X], self.li[_Y][_X] = self.li[_Y][_X], self.li[Y][X]
+                self.Y = _Y
+                self.X = _X
+
+        def Shoot():
+            _Y = Y
+            _X = X
+
+            arrow = {"^": (-1, 0), "v": (1, 0), "<": (0, -1), ">": (0, 1)}
+
+            add_Y, add_X = arrow[self.li[Y][X]]
+
             while 0 <= _Y + add_Y < H and 0 <= _X + add_X < W:
-                if li[_Y + add_Y][_X + add_X] == "*":
-                    li[_Y + add_Y][_X + add_X] = "."
+                if self.li[_Y + add_Y][_X + add_X] == "*":
+                    self.li[_Y + add_Y][_X + add_X] = "."
                     break
-                elif li[_Y + add_Y][_X + add_X] == "#":
+                elif self.li[_Y + add_Y][_X + add_X] == "#":
                     break
                 _Y += add_Y
                 _X += add_X
 
-        if not Bomb:
-            if li[_Y][_X] == ".":
-                li[Y][X], li[_Y][_X] = li[_Y][_X], li[Y][X]
-                Y = _Y
-                X = _X
+        mode = {"U": Up, "D": Down, "L": Left, "R": Right, "S": Shoot}
 
-        # print(act[i])
-        # for li_row in li:
-        #     print(*li_row)
-        # try:
-        #     print(act[i+1])
-        # except:
-        #     print("None")
-        # print()
+        H = self.H
+        W = self.W
 
-    print(f"#{case + 1} ",end="")
-    for r in range(H):
-        for c in range(W):
-            print(li[r][c], end="")
-        print()
+        for i in range(N):
+            X = self.X
+            Y = self.Y
+            mode[act[i]]()
+
+            print(act[i])
+            for li_row in self.li:
+                print(*li_row)
+            try:
+                print(act[i+1])
+            except:
+                print("None")
+            print()
+
+        # print(f"#{t + 1} ",end="")
+        # for r in range(H):
+        #     for c in range(W):
+        #         print(game.li[r][c], end="")
+        #     print()
+    
+    
+
+T = int(input())
+
+for case in range(T):
+    Height, Width = map(int, input().split())
+    game = Game(Height, Width)
+
+    Num = int(input())
+    acting = input()
+    
+    game.Command(Num, acting, case)
