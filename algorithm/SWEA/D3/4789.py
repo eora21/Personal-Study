9650901814a909777be6c89b18a2f18fe9f4016c
eora21 <eora21@naver.com>
2021-08-29 23:37:33 +0900

    4789

---
+++ algorithm/SWEA/D3/4789.py
@@ -0,0 +1,13 @@
+for case in range(int(input())):
+    people = list(map(int, input()))
+
+    now = 0
+    hire = 0
+
+    for i in range(len(people)):
+        if people[i] and now < i:
+            hire += i - now
+            now += hire
+        now += people[i]
+    
+    print("#{} {}".format(case + 1, hire))
